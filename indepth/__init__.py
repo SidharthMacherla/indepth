7484d7addd738056eabe94db4c41a159555299fe
Sidharth Macherla <msidharthrasik@gmail.com>
2020-09-12 19:05:23 +1200

    Update __init__.py

--- indepth/__init__.py
+++ indepth/__init__.py
@@ -3,8 +3,6 @@
 # Invoke Libraries
 #&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 import pandas as pd
-import os
-import warnings
 from indepth.functions import remov_punct, symSentSim  
 
 #&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
@@ -14,9 +12,7 @@ from indepth.functions import remov_punct, symSentSim
 # for a pair of document and query, the top sentences with high symmetric sentence similarity are returned.
 def MostSimilarSent(d, q, num_sents):
     lcase = d.lower()
-    sentences = lcase.split('.')
-    if(num_sents > len(sentences)):
-        warnings.warn("num_sents is more than the total number of sentences in the document. Hence num_sents is set to total number of sentences.")
+    sentences = lcase.split('.')    
     q = remov_punct(q)
     SSSlist = [(s, symSentSim(s,q)) for s in sentences if s]
     df = pd.DataFrame(SSSlist,columns = ['sentence','SSScore'])
