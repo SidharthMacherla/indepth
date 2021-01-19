4c0154b61dc4685ae0659f0f3d74ff8d6508045e
SidharthMacherla <msidharthrasik@gmail.com>
2021-01-20 11:19:20 +1300

    no message

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import indepth
 from indepth import *
 
-print(MostSimilarSent("where there is a will, there is a way", "way", 1))
 print(buildFeatures("This is a sample text. The main purpose of this sample is to test how things work."))
+print(MostSimilarSent("where there is a will, there is a way", "way", 1))
